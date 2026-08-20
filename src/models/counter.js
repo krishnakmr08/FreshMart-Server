9f28ec44869c5ceb9f597ad5993d8d2b2a3eb65f
Krishna Kumar <kkbhagat20@gmail.com>
2026-08-20 14:22:41 +0530

    feat: add MongoDB data models

---
+++ src/models/counter.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const counterSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    sequence_value: { type: Number, default: 0 },
+  },
+  { timestamps: true },
+);
+
+const Counter = mongoose.model("Counter", counterSchema);
+
+export default Counter;
